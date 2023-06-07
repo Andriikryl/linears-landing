938c55987187e451c7766316b12522a2a6d55dc8
Andriikryl <andreyk557@gmail.com>
2023-06-07 17:57:37 +0300

    add images

--- components/Hearo.tsx
+++ components/Hearo.tsx
@@ -6,10 +6,10 @@ interface HeroElementProps {
   children: React.ReactNode;
 }
 export const HeroTitle = ({ children }: HeroElementProps) => {
-  return <h1 className="text-5xl">{children}</h1>;
+  return <h1 className="text-5xl my-6">{children}</h1>;
 };
 export const HeroSubTitle = ({ children }: HeroElementProps) => {
-  return <h1 className="text-lg">{children}</h1>;
+  return <h1 className="text-lg mb-12">{children}</h1>;
 };
 export const Hero = ({ children }: HeroProps) => {
   return <div className=" text-center">{children}</div>;
