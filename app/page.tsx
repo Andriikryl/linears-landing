93f02f0a26d1b1e22766dfeb87994c812d531e7f
Andriikryl <andreyk557@gmail.com>
2023-06-07 17:48:41 +0300

    add title and subtitle components

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { Container } from "@/components/Container";
-import { Hero } from "@/components/Hearo";
+import { Hero, HeroSubTitle, HeroTitle } from "@/components/Hearo";
 
 export default function Home() {
   return (
@@ -9,10 +9,13 @@ export default function Home() {
       </header>
       <main>
         <Container>
-          <Hero 
-          title="Liniar is a better way to build products" 
-          subtitle="Meet the new standart for modern softwere development. Streamline issues , sprints, and product roadmaps"
-          />
+          <Hero>
+            <HeroTitle>Liniar is a better way to build products</HeroTitle>
+            <HeroSubTitle>
+              Meet the new standart for modern softwere development. Streamline
+              issues , sprints, and product roadmaps
+            </HeroSubTitle>
+          </Hero>
         </Container>
       </main>
       <footer>
--- components/Hearo.tsx
+++ components/Hearo.tsx
@@ -1,13 +1,16 @@
+import React from "react";
 interface HeroProps {
-  title: string;
-  subtitle: string;
+  children: React.ReactNode;
 }
-
-export const Hero = ({ title, subtitle }: HeroProps) => {
-  return (
-    <div>
-      <h1>{title}</h1>
-      <p>{subtitle}</p>
-    </div>
-  );
+interface HeroElementProps {
+  children: React.ReactNode;
+}
+export const HeroTitle = ({ children }: HeroElementProps) => {
+  return <h1 className="text-5xl">{children}</h1>;
+};
+export const HeroSubTitle = ({ children }: HeroElementProps) => {
+  return <h1 className="text-lg">{children}</h1>;
+};
+export const Hero = ({ children }: HeroProps) => {
+  return <div className=" text-center">{children}</div>;
 };
