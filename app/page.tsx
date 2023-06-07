c5b6470461b6446538f3d908a0c23ab32069e40e
Andriikryl <andreyk557@gmail.com>
2023-06-07 17:25:11 +0300

    add Hero title and subtitle

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { Container } from "@/components/Container";
+import { Hero } from "@/components/Hearo";
 
 export default function Home() {
   return (
@@ -7,7 +8,12 @@ export default function Home() {
         <Container>header</Container>
       </header>
       <main>
-        <Container>main</Container>
+        <Container>
+          <Hero 
+          title="Liniar is a better way to build products" 
+          subtitle="Meet the new standart for modern softwere development. Streamline issues , sprints, and product roadmaps"
+          />
+        </Container>
       </main>
       <footer>
         <Container>footer</Container>
