2bacb3fb1a01ae2aea2dcb5e8f113967904dc844
Andriikryl <andreyk557@gmail.com>
2023-06-08 18:46:02 +0300

    add global css styles

--- components/Hearo.tsx
+++ components/Hearo.tsx
@@ -6,7 +6,7 @@ interface HeroElementProps {
   children: React.ReactNode;
 }
 export const HeroTitle = ({ children }: HeroElementProps) => {
-  return <h1 className="text-5xl my-6">{children}</h1>;
+  return <h1 className="text-5xl md:text-7xl my-6">{children}</h1>;
 };
 export const HeroSubTitle = ({ children }: HeroElementProps) => {
   return <h1 className="text-lg mb-12">{children}</h1>;
